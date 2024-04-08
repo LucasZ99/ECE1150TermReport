4c1d335c7448633f31e61d712d1ce3f0cb1479a5
LucasZ99 <133819078+LucasZ99@users.noreply.github.com>
2024-04-07 20:11:57 -0400

    Add files via upload

---
+++ total_sim_code.m
@@ -0,0 +1,32 @@
+model = 'BCH_sim.slx';
+open_system(model);
+
+% compare theoretical error rate vs simulation error rate
+EbNoVec = 0:2:10;
+
+% Uncoded BER
+uncodedBER = berawgn(EbNoVec, "qam", 8);
+
+% theorBER will change depending on what kind of code you are generating
+theorBER = bercoding(EbNoVec,'block','hard',7,4,3);
+
+berVecBSC  = zeros(length(EbNoVec),3);
+for n   = 1:length(EbNoVec)
+    EbNo = EbNoVec(n);
+    sim(model);
+    berVecBSC(n,:) = berBSC(end,1);
+end
+
+% produce a semilog graph of coded, theoretical, and uncoded BER's
+semilogy( ...
+    EbNoVec,berVecBSC(:,1),'d', ...
+    EbNoVec,theorBER,'-',...
+    EbNoVec, uncodedBER, '-');
+
+legend('Simulated BSC BER','Theoretical BER','Uncoded BER',...
+    Location="southwest");
+
+xlabel("Eb/N0 (dB)");
+ylabel("Error Probability");
+title("Bit Error Probability");
+grid on;
